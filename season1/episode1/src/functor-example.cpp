34ee1184198a7795253c901280422b7f971bedff
Minh Bui <minh.tat.bui@gmail.com>
2014-11-11 00:12:41 +0100

    Edited source layout

--- season1/episode1/src/functor-example.cpp
+++ season1/episode1/src/functor-example.cpp
@@ -52,41 +52,51 @@ bool Compare(const string & str) {
 int main() {
     
     int n;
-    cout << "1. Function pointer\n2. Function object\n3. Lmbda function\n" << "Presse a number>";
+
+    cout << "Email address lookup program using:\n";
+    cout << "1. Function pointer\n2. Function object\n";
+    cout << "3. Lambda function\nPress a number>";
     cin >> n;
 
-    vector<string> addresses = {"Arash Shafiei", "a.shafiei@tue.nl", "arash.shafiei@gmail.com", "arash.shafiei@alumni.enseeiht.fr"}; 
+    vector<string> addresses = {"Arash Shafiei", 
+				"a.shafiei@tue.nl", 
+				"arash.shafiei@gmail.com", 
+				"arash.shafiei@alumni.enseeiht.fr"}; 
     AddressBook adrbook;
-    vector<string> result;
+    vector<string> results;
 
     switch(n) {
     case 1:
         // First solution with function pointers
-        result = adrbook.findAddr(addresses, Compare);
+        results = adrbook.findAddr(addresses, &Compare);
         // or
-        //result = adrbook.findAddr<bool (const string &)>(addresses, Compare);
-        break; 
+        //results = adrbook.findAddr<bool (const string &)>(addresses, Compare);
+        break;
+ 
     case 2:
         // Second solution with functor
         Comparator comp;    
-        result = adrbook.findAddr(addresses, comp);
+        results = adrbook.findAddr(addresses, comp);
         // or 
-        //result = adrbook.findAddr<Comparator>(addresses, comp);
+        //results = adrbook.findAddr<Comparator>(addresses, comp);
         break;
+
     case 3:
-        // Third solution with lambda
-        result = adrbook.findAddr(addresses, [](const string & str) {return str.find("@") != string::npos;});
+        // Third solution with lambda function
+        results = adrbook.findAddr(addresses, 
+				   [](const string & str) {return str.find("@") != string::npos;});
         // or 
-        //result = adrbook.findAddr<bool (const string &)>(addresses, [](const string & str) {return str.find("@") != string::npos;});
+        //results = adrbook.findAddr<bool (const string &)>(addresses, [](const string & str) {return str.find("@") != string::npos;});
         // or       
-        //result = adrbook.findAddr<function<bool (const string &)>>(addresses, [](const string & str) {return str.find("@") != string::npos;});
+        //results = adrbook.findAddr<function<bool (const string &)>>(addresses, [](const string & str) {return str.find("@") != string::npos;});
        
         break;
+
     default:
         cout << "Sorry the number is invalid.\n";
     
     }
 
-    for_each(result.begin(), result.end(), [](string val){cout << val << endl;});
+    for_each(results.begin(), results.end(), [](string val){cout << val << endl;});
 }
 
