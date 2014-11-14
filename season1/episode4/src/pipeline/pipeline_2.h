267e34735024b58e4cbddd6f954a501cfbcf8b0b
Arash Shafiei <arash.shafiei@gmail.com>
2014-11-14 21:09:48 +0100

    refactoring

--- season1/episode4/src/pipeline/pipeline_2.h
+++ season1/episode4/src/pipeline/pipeline_2.h
@@ -1,20 +1,3 @@
-/*
- * Copyright 2014 <copyright holder> <email>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
-
 #ifndef PIPELINE_2_H
 #define PIPELINE_2_H
 
@@ -24,7 +7,6 @@
 #include <string>
 #include <vector>
 #include <mutex>
-#include "semaphore.h"
 
 const int SIZE = 10;
 
@@ -98,8 +80,6 @@ public:
   }
   
   int getSize() const {return size;}
-  
-  Buffer<T>& operator[](std::size_t i) = delete;
   
   Node<T>& at(int idx) const  { return nodes[idx]; }
 };
